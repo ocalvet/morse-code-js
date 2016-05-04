2da43b14a9d1dba3b90761321ee603c51d77e6bb
Ovidio R. Calvet <calveto@gmail.com>
2016-05-03 22:54:46 -0400

    Added files to run the js in the module

---
+++ app/app.js
@@ -0,0 +1,9 @@
+export default class {
+  constructor() {
+    console.log('application created');
+  }
+
+  initialize() {
+    console.log('application initialized');
+  }
+}
---
+++ startup.js
@@ -0,0 +1,5 @@
+System.import('app/app').then(App  => {
+  let app = new App();
+  app.initialize();
+  console.log('app started')
+});
