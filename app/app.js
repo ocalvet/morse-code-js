946c6ddd7442e770155270941679e09d2e1d454b
Ovidio R. Calvet <calveto@gmail.com>
2016-05-05 10:27:00 -0400

    Added a strict mode and some comments

--- app/app.js
+++ app/app.js
@@ -6,9 +6,13 @@ export class App {
   }
 
   initialize() {
+    // Get a reference to the button 
     let button = this.document.getElementById('code-btn');
     const values$ = Rx.Observable
       .fromEvent(button, 'click')
-      .subscribe((event) => console.log('event ', event));
+      .subscribe(
+          (event) => console.log('event ', event), 
+          (error) => console.log(error)
+      );
   }
 }
--- startup.js
+++ startup.js
@@ -1,3 +1,5 @@
+'use strict';
+
 System.import('app/app').then(application  => {
   let app = new application.App(window.document);
   app.initialize();
