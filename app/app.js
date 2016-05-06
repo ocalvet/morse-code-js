cafdb4248a5950fff1d6fbc10f7fa7e7c4cfb3ec
Ovidio R. Calvet <calveto@gmail.com>
2016-05-05 22:42:11 -0400

    Adding a long click observable

--- app/app.js
+++ app/app.js
@@ -9,10 +9,12 @@ export class App {
     // Get a reference to the button 
     let button = this.document.getElementById('code-btn');
     const values$ = Rx.Observable
-      .fromEvent(button, 'click')
+      .fromEvent(button, 'mousedown')
+      .delay(300)
+      .takeUntil(button, 'mouseup')
       .subscribe(
           (event) => console.log('event ', event), 
           (error) => console.log(error)
       );
   }
-}
+} 
