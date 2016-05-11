568628633c2676de675e4bb139d2c88f0efcd679
Ovidio R. Calvet <calveto@gmail.com>
2016-05-11 01:05:08 -0400

    Added lon click stream

--- app/app.js
+++ app/app.js
@@ -9,20 +9,25 @@ export class App {
     // Get a reference to the button 
     let button = this.document.getElementById('code-btn');
     
-    let longClick$ = Rx.Observable.fromEvent(button, 'mousedown')
-        .flatMap((e) => {
-            return Rx.Observable
-                .return(e)
-                .delay(200)
-                .takeUntil(Rx.Observable.fromEvent(button, 'mouseup'))
-        });
+    let longClick$ =  Rx.Observable
+                        .fromEvent(button, 'mousedown')
+                        .flatMap((e) => {
+                            return Rx.Observable
+                                .return(e)
+                                .delay(500)
+                                .takeUntil(Rx.Observable.fromEvent(button, 'mouseup'))
+                        });
     
     longClick$.subscribe((l) => {
-        longclicked = true;
+        console.log('longclick happened');
     });
     
-    const values$ = Rx.Observable
-      .fromEvent(button, 'mousedown')
+    let normalClick = Rx.Observable
+                        .fromEvent(button, 'mousedown')
+                        .skipUntil(Rx.Observable.fromEvent(button, 'mouseup'))
+                        .subscribe((e) => {
+                            console.log('Short click happened', e);
+                        });
     
   }
 } 
